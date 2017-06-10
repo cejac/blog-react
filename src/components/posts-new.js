3373e65a5a444b22b384d2e0695a5f99d6c31434
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-06-09 18:48:22 -0700

    the field component

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+renderTitleField(field) {
+  return (
+    <div>
+      <input
+        type='text'
+        {...field.input}
+      />
+    </div>
+  )
+}
+
   render() {
     return (
       <form>
         <Field
           name='title'
-          component={}
+          component={this.renderTitleField}
         />
       </form>
     )
   }
 }
 
-export default reduxform({
-  form: 'PostsNewForm' //make sure this is unique
+export default reduxForm({
+  form: 'PostsNewForm' //make sure this is unique so it does not share state with any other forms
 })(PostsNew);
