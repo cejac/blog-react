e804e9a1774971b4f194f2a67429cc7fba5e7d4b
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-06-09 18:30:55 -0700

    setting up a redux form

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
     return (
-      <div>
-        Posts New!
-      </div>
+      <form>
+        <Field
+          name='title'
+          component={}
+        />
+      </form>
     )
   }
 }
 
-export default PostsNew;
+export default reduxform({
+  form: 'PostsNewForm' //make sure this is unique
+})(PostsNew);
